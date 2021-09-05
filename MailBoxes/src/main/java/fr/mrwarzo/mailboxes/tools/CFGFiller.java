dbc150b418faeb568c997b2aef62af2c450ee601
- width : Integer     - length : Integer     + Graph(width, length) <jordan.pereira@etu.unistra.fr>
2021-09-05 14:13:12 +0200

    Suppression du projet dupliqué

--- MailBoxes/src/main/java/fr/mrwarzo/mailboxes/tools/CFGFiller.java
+++
@@ -1,7 +0,0 @@
-package fr.mrwarzo.mailboxes.tools;
-
-import org.bukkit.configuration.file.FileConfiguration;
-
-public interface CFGFiller {
-    void fill(FileConfiguration fileConfiguration);
-}
